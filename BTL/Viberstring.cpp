0cb2fdf9a642fb34555419f0d0946a94fab1992c
trongnd32 <trong.nd.32@gmail.com>
2019-11-27 18:46:30 +0700

    update result full step

--- BTL/Viberstring.cpp
+++ BTL/Viberstring.cpp
@@ -8,18 +8,29 @@
 
 using namespace std;
 
+void Write2File(float *A, int row, int col) {
+  FILE *result = fopen("ViberStringC1_MPI.txt", "a");
+  int i, j;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      fprintf(result, "%8.5lf", *(A + i * col + j));
+    }
+    fprintf(result, " ");
+  }
+  fclose(result);
+}
+
 int main(int argc, char *argv[]) {
   int rank, size;
   MPI_Init(&argc, &argv);
-  //
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Status stat;
   
   // const
-  int M = 20;
+  int M = 200;
   float dt = 0.01;
-  float Time = 1.0/2;
+  float Time = 0.15;
   float dx = 1.0 / (M - 1);
   float C = 1;
   float k = 1;
@@ -39,13 +50,16 @@ int main(int argc, char *argv[]) {
     Phi_old[i] = Phi[i];
   }
 
+  // print result to file
+  FILE *resultFile = fopen("ViberStringC1_MPI.txt", "a");
+
   // calculate
   int N = Time/dt;
 
   if (rank < size - 1) MPI_Send(&Phi[Mc - 1], 1, MPI_FLOAT, rank + 1, 2 * (rank + 1), MPI_COMM_WORLD);
   if (rank > 0) MPI_Send(&Phi[0], 1, MPI_FLOAT, rank - 1, 2 * (rank - 1) + 1, MPI_COMM_WORLD);
 
-  for(int t = 0;t < N;t++){
+  for(int t = 0; t < N; t++){
     float L = 0, R = 0;
     if(rank > 0) MPI_Recv(&L, 1, MPI_FLOAT, rank - 1, 2 * rank + t * size * 2, MPI_COMM_WORLD, &stat);
     if (rank < size - 1) MPI_Recv(&R, 1, MPI_FLOAT, rank + 1, 2 * rank + 1 + t * size * 2, MPI_COMM_WORLD, &stat);
@@ -59,24 +73,33 @@ int main(int argc, char *argv[]) {
 
       Phi_new[i] = 2 * c - Phi_old[i] + tau * tau * (l - 2 * c + r);
     }
-    for(int i=0;i<Mc;i++) {
+    for(int i = 0; i < Mc; i++) {
       Phi_old[i] = Phi[i];
       Phi[i] = Phi_new[i];
     }
 
     if (rank < size - 1) MPI_Send(&Phi[Mc - 1], 1, MPI_FLOAT, rank + 1, 2 * (rank + 1) + (t + 1) * size * 2, MPI_COMM_WORLD);
     if (rank > 0) MPI_Send(&Phi[0], 1, MPI_FLOAT, rank - 1, 2 * (rank - 1) + 1 + (t + 1) * size * 2, MPI_COMM_WORLD);
+
+    //write step by step to file
+    MPI_Gather(Phi, Mc, MPI_FLOAT, result, Mc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    if(rank == 0) {
+      for (int i = 0; i < M; i++) {
+        fprintf(resultFile, "%8.4lf ", *(result + i));
+      }
+      if(t < N - 1) fprintf(resultFile, "%s", ";\n");
+    }
   }
 
-  MPI_Gather(Phi, Mc, MPI_FLOAT, result, Mc, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
   // print result
-  if(rank == 0){
-    for(int i = 0; i < M;i++){
-      printf("%f\n", result[i]);
-    }
-    printf("\n");
-  }
+  // if(rank == 0){
+  //   for(int i = 0; i < M;i++){
+  //     printf("%f\n", result[i]);
+  //   }
+  //   printf("\n");
+  //   Write2File(result,M,1);
+  // }
 
   MPI_Finalize();
 }
